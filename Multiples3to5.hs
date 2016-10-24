9dc106caeb0f630cc160d45dc5fb27a8ad706ad1
Diego Rubio <diegodarm9419@gmail.com>
2016-10-24 16:03:34 -0700

    Update Multiples3to5.hs
    comentarios

--- Multiples3to5.hs
+++ Multiples3to5.hs
@@ -1,2 +1,10 @@
 main = putStrLn (show respuesta)
+--en esta variabel llamada respuesta vemos como solicita la suma de los numeros de una lista X donde X va estar 
+--dentro de un rango de una lista de 0 a 999 y ademas solo mostrara el residuo(mod) de 3 o 5 como se muestra 
 respuesta = sum [x | x <- [0..999], (mod x 3) == 0 || (mod x 5) == 0]
+--a su vez si se desea ver solamene la lista de numeros si retiramos la suma de la lista(sum) nos mostrara dicha lista
+--para eso lo realizamos como el siguiente codigo 
+-- let a = [x | x <- [0..999], (mod x 3) == 0 || (mod x 5) == 0]
+-- a  
+
+
